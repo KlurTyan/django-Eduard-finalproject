00191e8f53d0491f1bc520fa1dcf77871011d99c
Eduard Tyan <rusgaymtuan@gmail.com>
2023-05-05 20:46:10 +0600

    Django project

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from .views import index, about
+from .views import index, about, post_single
 
 urlpatterns = [
     path('', index, name='home'),
-    path('about', about, name='about')
-]
+    path('about', about, name='about'),
+    path('<int:pk>/', post_single, name = 'single')
+] 
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post, AboutUS
 
 def index(request):
@@ -8,3 +8,8 @@ def index(request):
 def about(request):
     aboutPost = AboutUS.objects.last()
     return render(request, 'about.html', {'about' : aboutPost})
+
+def post_single(request, pk):
+    # Post.objects.get(pk=pk)
+    p = get_object_or_404(Post.objects.all(), pk=pk)
+    return render(request, 'post_single.html', {'post' : p})
